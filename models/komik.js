ea0e855c9fe5c2d35200eac826949e90ca5395ac
Fannandya Sutan <sfannandya@gmail.com>
2026-07-20 10:23:27 +0700

    membuat models untuk tabel Komik

---
+++ models/komik.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Komik = sequelize.define("Komik", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },  
+        author: {
+            type: DataTypes.STRING, 
+
+        }
+    });
+    return Komik;
+};
